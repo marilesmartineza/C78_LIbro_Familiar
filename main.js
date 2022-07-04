cb4ce09a4cc1c29107157ae12a6db7f21d3c43c5
Ale <97320635+marilesmartineza@users.noreply.github.com>
2022-07-04 18:14:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
-var updateImage =[images];
-var updateName =[names];
+    var images=[
+    src="portada.jpg",
+    src="Ma.jpg",
+    src="Pa.jpg",
+    src="Yop.jpg",
+    src="Bro.jpg",
+
+];
+
+
+var updateName =["Yazmín ","Jorge", "Alexandra","Jorge"];
+
+console.log(updateName);
+
+
+
+var i = 0;
+function update() { 
+ 
+   if(i == 5)
+     {
+       i=0;
+     }
+   
+    document.getElementById("album").src = images[i];
+    document.getElementById("nombre").src = updateName[i];
+  i++;
+ 
+}
